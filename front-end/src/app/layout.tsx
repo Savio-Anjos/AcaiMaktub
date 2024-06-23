55d2918b8b7baee20d10d4ae4eb866518ed9e97b
José Leandro <juniorctba1@gmail.com>
2024-06-23 19:13:14 -0300

    feat :sparkles: AddedHome Page

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
-import { store } from "@/store";
 import { ReduxProvider } from "./providers/reduxProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Açaiteria",
@@ -18,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={montserrat.className}>
         <ReduxProvider>{children}</ReduxProvider>
       </body>
     </html>
--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,4 +1,30 @@
+import styles from "./page.module.css";
+import Image from "next/image";
 import { Header } from "./components/Header";
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main className={styles.container}>
+        <section className={styles.content}>
+          <div className={styles.title}>
+            <div className={styles.wordWaves}>
+              <span>Açaiteria</span>
+              <span>Açaiteria</span>
+            </div>
+          </div>
+          <h3>
+            Os melhores açaís, com sabores únicos e frescor incomparável, você
+            só encontra aqui!
+          </h3>
+
+          <button className={styles.button}>Ver Produtos</button>
+        </section>
+
+        <section className={styles.image}>
+          <Image alt="Açais" src="/acais.jpg" width={470} height={660} />
+        </section>
+      </main>
+    </>
+  );
 }
